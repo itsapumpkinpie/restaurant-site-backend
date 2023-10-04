87cfb2dc2d3925597a7043d3744947e41e76e615
itsapumpkinpie <itsapumpkinpie@gmail.com>
2023-10-04 15:01:10 +0300

    connect to db

---
+++ menu/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Menu
+
+# Register your models here.
+admin.site.register(Menu)
+
+    
---
+++ menu/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Menu(models.Model):
+    dish_name = models.CharField(max_length=50)
+    photo_path = models.CharField(blank=False)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.dish_name
+
+class Category(models.Model):
+    cat_name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.cat_name
