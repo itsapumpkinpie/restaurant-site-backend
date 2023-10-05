bbcc944efd4cee35327a2d382da363f23dd053b7
itsapumpkinpie <itsapumpkinpie@gmail.com>
2023-10-05 12:16:40 +0300

    testing api

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Menu
+from .serializers import MenuSerializer
 
-# Create your views here.
+
+class MenuAPIView(generics.ListAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
